b7e6cde6e8d26683ac0fa6548ae6dc90c95405b9
Gabriel fernandes <Gabrielfernandesfotografias@outlook.com>
2022-03-05 02:56:19 -0300

    update next Api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.statusCode = 200
+  res.json({
+    name: 'Gabriel fernandes',
+    age: 34,
+    hobbies: ['fotografias', 'patins', 'codar'],
+  })
 }
